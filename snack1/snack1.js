fa0ec715130b2de6780b3822616b2eb5a191cca5
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-04-15 16:36:07 +0900

    Snack1 ultimato

--- snack1/snack1.js
+++ snack1/snack1.js
@@ -13,7 +13,8 @@ do
 {
     let bike_n = prompt("Inserisci il nome della bicicletta oppure stringa vuota per concludere l'input");
     let bike_w = parseFloat(prompt("Inserisci il peso della bicicletta oppure 0 per concludere l'input"));
-    ((bike_n != "") && (bike_w != 0)) ? data_array.push({bike_n, bike_w}) : finished = true;
+    // Inserimento dati nell'array in maniera destrutturata
+    ((bike_n != "") && (bike_w != 0) && (!isNaN(bike_w))) ? data_array.push({bike_n, bike_w}) : finished = true;
     if (!finished) console.log(data_array[data_array.length-1]);
 } while (!finished);
 
@@ -25,8 +26,6 @@ if (data_array.length == 0)
 }
 else
 {
-    // Variabile che, a fine ciclo di ordinamento, conterrà il peso minimo (che potrebbe anche essere comune a più biciclette)
-    let min_weight = 0;
     // Logica nel caso di presenza di almeno un oggeto valido all'interno dell'array
     // 2 Cicli for nidificati per ordinare l'array in senso di peso bicicletta crescente 
     for (let i = 0; i < data_array.length - 1; i++)
@@ -42,6 +41,32 @@ else
         }
     }
     console.log("array ordinato: ",data_array);
+    // Si salva il peso minimo e attraverso filter si crea un array con tutte le biciclette di peso minimo
+    const min_weight = data_array[0]["bike_w"];
+    const min_weight_array = data_array.filter((element) => 
+    {
+        // Destrutturazione oggetto dentro il metodo filter
+        const {bike_n, bike_w} = element;
+        if (bike_w === min_weight) return true;
+    });
+    console.log("array delle biciclette più leggere: ", min_weight_array);
+
+    // Si creano i messaggi in output mediante manipolazione del DOM con nidificazione di elementi, ricorso al metodo forEach, destrutturazione oggetto e template literal
+    const message = document.createElement("div");
+    message.innerHTML = `
+    <h3>Il peso minore tra tutti quelli inseriti è di ${min_weight_array[0].bike_w} kg</h3>
+    <ul>Il numero di biciclette del peso sopra indicato è: ${min_weight_array.length}...</ul>`;
+    min_weight_array.forEach((element) => 
+    {
+        // Destrutturazione oggetto dentro il metodo forEach
+        const {bike_n, bike_w} = element;
+        // Si creano tanti list item quante sono le biciclette dal peso minimo e li si collega alla lista appena generate dentro un innerHTML
+        let bicicletta = document.createElement("li");
+        bicicletta.innerText = `${bike_n} - peso: ${bike_w} kg`;
+        message.querySelector("ul").append(bicicletta);
+    });
+    const doc_main = document.querySelector("main");
+    doc_main.append(message);
 }
 
 
